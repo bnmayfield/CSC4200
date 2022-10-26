9c9307eb6c971b072150d28b60df7c6f87771eb7
bnmayfield <78820413+bnmayfield@users.noreply.github.com>
2022-10-26 18:42:57 -0500

    Class Wprk 10/26/22

--- client.py
+++ client.py
@@ -6,18 +6,24 @@ sock = socket.socket()
 print ("Socket successfully created")
 server = sys.argv[1]
 port = int(sys.argv[2])
-#logFile = sys.argv[3]
+logFileName = sys.argv[3]
 print('This is the server input:', server )
 print('This is the port input:', port )
+print('This is the log file name input:', logFileName)
+
+logFile = open(logFileName, "a")
 
 sock.connect((server, port))  
 
 print("Please enter the message for the server. HINT: Please include the word 'Network'")
 clientMessage= input() 
-#print(clientMessage)  
-#message from user
 sock.send(clientMessage.encode())
+logFile.write('Client: '+ clientMessage +'\n')
+
 serverMessage= sock.recv(1024).decode()
 print (serverMessage)
+logFile.write('Server: '+ serverMessage +'\n')
+
+logFile.close()
 sock.close()
 print("Socket Closed")
