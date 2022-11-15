7a2d8375a76232e4e8bb39e869d5214983ea48ff
bnmayfield <78820413+bnmayfield@users.noreply.github.com>
2022-11-14 18:01:03 -0600

    11/14/22 6:00pm

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
 import socket
+import sys
+
+
+port = int(sys.argv[1])
+logFileName = sys.argv[2]
 
 sock = socket.socket()
 print ("Socket made")
-
-port=8001
 
 sock.bind(('', port))
 
 sock.listen(5)
 print("this sock be listening")
 
+#Read in the quote text file(?)
+
 while True:
-    client, adress = sock.accep()
+    client, address = sock.accept()
+    print("Got an address: ", address)
+    clientMessage= sock.recv(1024).decode()
     client.send("thanks for connecting")
     client.close()
     break
