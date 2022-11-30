7dc6a41f17617955abfb30cc8a63d5aa0a5ba3f7
bnmayfield <78820413+bnmayfield@users.noreply.github.com>
2022-11-29 18:41:38 -0600

    First Section Done

--- client.py
+++ client.py
@@ -1,8 +1,24 @@
 import sys
 import socket
-#35.225.41.220
+import struct
+#34.173.101.159
 #8001
-# python3 client.py 35.225.41.220 8001 logFile.txt
+
+#INSTANCE 2 SERVER
+# python3 client.py 34.173.101.159 8001 logFile.txt
+
+#packet section
+h_message = 'Hello'
+packet_hello = struct.pack('!III',17,1,len(h_message))
+packet_hello += h_message.encode()
+#print('Hello Packet: ',packet_hello)
+c_message = 'LIGHTON'
+packet_command = struct.pack('!III',17,2,len(c_message))
+packet_command += c_message.encode()
+#print('Command Packet: ',packet_command)
+
+
+
 sock = socket.socket()
 print ("Socket successfully created")
 server = sys.argv[1]
@@ -12,19 +28,19 @@ print('This is the server input:', server )
 print('This is the port input:', port )
 print('This is the log file name input:', logFileName)
 
-logFile = open(logFileName, "a")
+#logFile = open(logFileName, "a")
 
 sock.connect((server, port))  
+sock.sendall(packet_hello)
+#print("Please enter the message for the server. HINT: Please include the word 'Network'")
+#clientMessage= input() 
+#sock.send(clientMessage.encode())
+#logFile.write('Client: '+ clientMessage +'\n')
 
-print("Please enter the message for the server. HINT: Please include the word 'Network'")
-clientMessage= input() 
-sock.send(clientMessage.encode())
-logFile.write('Client: '+ clientMessage +'\n')
+#serverMessage= sock.recv(1024).decode()
+#print (serverMessage)
+#logFile.write('Server: '+ serverMessage +'\n')
 
-serverMessage= sock.recv(1024).decode()
-print (serverMessage)
-logFile.write('Server: '+ serverMessage +'\n')
-
-logFile.close()
+#logFile.close()
 sock.close()
 print("Socket Closed")
--- server.py
+++ server.py
@@ -1,34 +1,56 @@
 import socket
 import sys
-import random
+#import random
+import struct
+
 # python3 server.py 8001 serverLogFile.txt
 #tcpdump -i ens4 dst 35.225.41.220 -w project2.pcap -c 20
+
+#packet section
+h_message = 'Hello'
+packet_hello = struct.pack('!III',17,1,len(h_message))
+packet_hello += h_message.encode()
+
+c_message = 'LIGHTON'
+packet_command = struct.pack('!III',17,2,len(c_message))
+packet_command += c_message.encode()
+
+
+
+#Reading arguments
 port = int(sys.argv[1])
 logFileName = sys.argv[2]
 
+
+#making socket
 sock = socket.socket()
 print ("Socket made")
-
 sock.bind(('', port))
-
 sock.listen(5)
 print("this sock be listening")
 
+#opening log file
 logFile = open(logFileName, "a")
+
+
 
 while True:
     client, address = sock.accept()
-    print("Got an address: ", address)
+    print("Received connection from: ", address)
+    logFile.write('Received Connection from: '+ str(address) +'\n')
 
-    clientMessage= str(client.recv(1024))
-    print(clientMessage)
-    logFile.write('Client: '+ clientMessage +'\n')
+    clientPacket= client.recv(struct.calcsize('!III'))
+    version, message_type, length = struct.unpack('!III',clientPacket)
+    print(version, message_type, length)
+    message= client.recv(length).decode()
+    print(message)
+    logFile.write('Client Packet (Version #, Message Type, Length, Message): '+ str(version)+','+ str(message_type)+','+str(length)+','+message+'\n')
 
-    serverMessage="Hello, here is a random quote for you: "
-    lines = open('quotes.txt').read().splitlines()
-    serverMessage = serverMessage + random.choice(lines)
-    client.send(serverMessage.encode())
-    logFile.write('Server: '+ serverMessage +'\n')
+   # serverMessage="Hello, here is a random quote for you: "
+    #lines = open('quotes.txt').read().splitlines()
+    #serverMessage = serverMessage + random.choice(lines)
+    #client.send(serverMessage.encode())
+   # logFile.write('Server: '+ serverMessage +'\n')
 
     client.close()
     print("Client Closed")
